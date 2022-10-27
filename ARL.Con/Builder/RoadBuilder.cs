df539e24335af0e8fd2b2256cf0898bda7a42346
ast <ast@htc-cs.com>
2022-10-27 18:19:49 +0400

    builder - draft

--- ARL.Con/Builder/RoadBuilder.cs
+++ ARL.Con/Builder/RoadBuilder.cs
@@ -1,4 +1,5 @@
-﻿using ARL.Core.Models.World;
+﻿using ARL.Core.Models;
+using ARL.Core.Models.World;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,12 @@ namespace ARL.Con.Builder;
 public class RoadBuilder : WorldBuilder
 {
     protected Road _road;
+
+    protected RoadBuilder(World world, Road road) : base(world)
+    {
+        _road = road;
+    }
+
     public RoadBuilder(World world, string fromid, string toid, int cost, int days, bool twoway) : base(world)
     {
         _road = new Road() { SafeTravelCost = cost, Days = days };
@@ -18,21 +25,43 @@ public class RoadBuilder : WorldBuilder
             _world.Roads[(toid, fromid)] = _road;
     }
 
-    public RoadBuilder AddEnemy(int propability, string name, string desc)
+    public RoadEnemyBuilder AddEnemy(int propability, string name, string desc)
     {
-        //...
-        return this;
+        var enemy = new RoadEnemy()
+        {
+            Probability = propability,
+            Name = name,
+            Description = desc,
+        };
+        _road.Events.Add(enemy);        
+        return new RoadEnemyBuilder(_world,_road,enemy);
     }
 
     public RoadBuilder AddTreasure(int propability, string name, string desc, string message, int start, int size)
     {
-        //...
+        _road.Events.Add(
+            new RoadTreasure()
+            {
+                Probability = propability,
+                Name = name,
+                Description = desc,
+                Message = message,
+                Treasure = new Drop() { Start = start, Interval = size}
+            }
+        );
         return this;
     }
 
     public RoadBuilder AddTrash(int propability, string name, string desc)
     {
-        //...
+        _road.Events.Add(
+            new RoadTrash()
+            {
+                Probability = propability,
+                Name = name,
+                Description = desc
+            }
+        );
         return this;
     }
 }
---
+++ ARL.Con/Builder/RoadEnemyBuilder.cs
@@ -0,0 +1,45 @@
+﻿using ARL.Core.Models;
+using ARL.Core.Models.World;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARL.Con.Builder;
+
+public class RoadEnemyBuilder : RoadBuilder
+{
+    RoadEnemy _enemy;
+
+    public RoadEnemyBuilder(World world, Road road, RoadEnemy enemy): base(world, road)
+    {
+        _enemy = enemy;
+    }
+
+    public RoadEnemyBuilder SetHP(int hp)
+    {
+        _enemy.HP = hp;
+        return this;
+    }
+
+    public RoadEnemyBuilder SetTreasure(int start, int size)
+    {
+        _enemy.Treasure = new Drop() { Start = start, Interval = size };
+        return this;
+    }
+
+    public RoadEnemyBuilder SetAttack(int attack, Dice damage)
+    {
+        _enemy.Attack = attack;
+        _enemy.Damage = damage;
+        return this;
+    }
+
+    public RoadEnemyBuilder SetDeffence(int defence, int protection)
+    {
+        _enemy.Defence = defence;
+        _enemy.Protection = protection;
+        return this;
+    }
+}
